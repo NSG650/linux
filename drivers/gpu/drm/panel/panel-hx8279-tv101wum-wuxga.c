da5bf368b9d313c04f2dae267106048f313d35e4
Bassusteur <42449683+bassusteur@users.noreply.github.com>
2024-05-21 21:01:06 +0200

    Update panel-hx8279-tv101wum-wuxga.c

--- drivers/gpu/drm/panel/panel-hx8279-tv101wum-wuxga.c
+++ drivers/gpu/drm/panel/panel-hx8279-tv101wum-wuxga.c
@@ -522,7 +522,7 @@ static int hx8279_tv101wum_wuxga_probe(struct mipi_dsi_device *dsi)
 	return 0;
 }
 
-static int hx8279_tv101wum_wuxga_remove(struct mipi_dsi_device *dsi)
+static void hx8279_tv101wum_wuxga_remove(struct mipi_dsi_device *dsi)
 {
 	struct hx8279_tv101wum_wuxga *ctx = mipi_dsi_get_drvdata(dsi);
 	int ret;
@@ -532,8 +532,6 @@ static int hx8279_tv101wum_wuxga_remove(struct mipi_dsi_device *dsi)
 		dev_err(&dsi->dev, "Failed to detach from DSI host: %d\n", ret);
 
 	drm_panel_remove(&ctx->panel);
-
-	return 0;
 }
 
 static const struct of_device_id hx8279_tv101wum_wuxga_of_match[] = {
